8099f509c236e67bc0fa5d6eead75dd11b86c993
makar <mmk@liis.su>
2022-02-20 02:13:30 +0300

    Init

---
+++ find-a-couple-game-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  find-a-couple-game-swiftui
+//
+//  Created by makar on 20.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ find-a-couple-game-swiftui/find_a_couple_game_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  find_a_couple_game_swiftuiApp.swift
+//  find-a-couple-game-swiftui
+//
+//  Created by makar on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct find_a_couple_game_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
